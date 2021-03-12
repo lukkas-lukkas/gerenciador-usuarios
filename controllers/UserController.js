2ea678ee564db4ed5a2aff8a24cc098c0889a316
lukkas-lukkas <66659328+lukkas-lukkas@users.noreply.github.com>
2021-03-12 17:58:13 -0300

    Criação da classe utils e inserção de data no cadastro de usuários

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -10,12 +10,18 @@ class UserController {
         this.formEl.addEventListener('submit', event=>{
             event.preventDefault();
 
+            let button = this.formEl.querySelector('[type=submit]');
+
+            button.disabled = true;
+
             let values = this.getValues();
 
             this.getPhoto().then(
                 (content)=>{
                     values.photo = content;
                     this.addLine(values);
+                    this.formEl.reset();
+                    button.disabled = false;
                 },
                 (erro)=>{
                     console.error(erro);
@@ -80,7 +86,7 @@ class UserController {
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
             <td>${dataUser.admin}</td>
-            <td>${dataUser.birth}</td>
+            <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
             <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
             <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
