32433fef88573c0732c1a3d8a001a4c0635ae30d
lukkas-lukkas <66659328+lukkas-lukkas@users.noreply.github.com>
2021-03-15 14:02:16 -0300

    Validação do formulário

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -33,8 +33,15 @@ class UserController {
     getValues(){
 
         let user = {};
+        let isValid = true;
 
         [...this.formEl.elements].forEach(field => {
+
+            if(['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value){
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+            }
+
             if(field.name == 'gender'){
                 if(field.checked){
                     user[field.name] = field.value;
@@ -45,8 +52,13 @@ class UserController {
                 user[field.name] = field.value;
             }
         });
-    
+
+        if(!isValid){
+            return false;
+        }
+
         return new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin);
+    
     }
 
     getPhoto(){
